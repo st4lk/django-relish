0f15a235b945cbcb024917a36401d6274ff73be2
st4lk <myhappydo@gmail.com>
2013-11-07 11:13:33 +0400

    decorator self_if_blank_arg

--- relish/decorators/__init__.py
+++ relish/decorators/__init__.py
@@ -1,1 +1,1 @@
-from .generic import instance_cache
+from .generic import *
--- relish/decorators/generic.py
+++ relish/decorators/generic.py
@@ -1,4 +1,10 @@
 from functools import wraps
+"""
+wraps helps to remember information about wrapped function,
+for example attributes like __name__, __doc__
+details: http://docs.python.org/2/library/functools.html#functools.wraps
+"""
+
 
 def instance_cache(func):
     """
@@ -33,4 +39,38 @@ def instance_cache(func):
             value = func(instance, *args, **kwargs)
             setattr(instance, attr_name, value)
             return value
+    return wrapped
+
+
+def self_if_blank_arg(func):
+    """
+    If all of function args and kwargs are False in python way, then
+    return self. Otherwise, return applied function. Useful for querysets.
+
+    Example:
+
+        from relish.decorators import self_if_blank_arg
+
+        class MyModel(QuerySet):
+            # to apply this queryset to model use PassThroughManager 
+            # from django-model-utils
+            # or see https://code.djangoproject.com/ticket/15062 for 
+            # solution without dependencies
+
+            @self_if_blank_arg
+            def in_country(self, country):
+                return self.filter(country=country)
+
+
+        # `country` variable contains country or None
+        # If None, country filter will NOT be applied
+        MyModel.objects.in_country(country)
+    """
+
+    @wraps(func)
+    def wrapped(instance, *args, **kwargs):
+        if any(args) or any(kwargs.values()):
+            return func(instance, *args, **kwargs)
+        else:
+            return instance
     return wrapped
