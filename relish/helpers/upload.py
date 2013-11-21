21d178b3fcefa13dc46d0fc7f9b3bbbfc6a27e66
st4lk <myhappydo@gmail.com>
2013-11-21 18:56:01 +0400

    upload_to: doc, add import in __init__.py

--- relish/helpers/upload.py
+++ relish/helpers/upload.py
@@ -4,6 +4,13 @@ import random
 import string
 
 def upload_to(path):
+    """
+    Generates unique ascii filename before saving.
+    Example:
+
+        class SomeModel(models.Model):
+            picture = models.ImageField(upload_to=upload_to('my_model_uploads/'))
+    """
     def upload_callback(instance, filename):
         random_fname = ''.join(
             random.choice(string.ascii_uppercase + string.digits) for x in range(16))
