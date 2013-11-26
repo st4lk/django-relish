a729c70f82e55ce5c7a6c6b924d37de9d2544953
st4lk <myhappydo@gmail.com>
2013-11-27 00:56:57 +0400

    fix show_age_ru: check, that input can be converted to int

--- relish/helpers/show.py
+++ relish/helpers/show.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 
+
 def show_age_ru(age):
     """
     Shows correct age (russian language)
     """
+    try:
+        age = int(age)
+    except TypeError:
+        return u""
+    except ValueError:
+        return u""
     last_age_digit = age % 10
     if last_age_digit == 1:
         unit = u'год'
