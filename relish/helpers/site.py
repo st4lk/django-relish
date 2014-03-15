8e6b6fdc939aa2a82bbf432eb6339b5b5a84cb9c
st4lk <myhappydo@gmail.com>
2014-03-15 16:34:03 +0400

    make get_domain more flexible

--- relish/helpers/site.py
+++ relish/helpers/site.py
@@ -1,4 +1,7 @@
 from django.contrib.sites.models import get_current_site
+from django.conf import settings
 
-def get_domain(request=None):
-    return "http://" + str(get_current_site(request))
+
+def get_domain(request=None, protocol=None):
+    protocol = protocol or getattr(settings, "SITE_PROTOCOL", None) or 'http'
+    return "{0}://{1}".format(protocol, get_current_site(request))
